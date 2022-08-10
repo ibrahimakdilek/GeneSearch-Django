b07bbe16bd838e58a7cf7c26aa28464fcfb93493
ibrahimakdilek <ibrahimakdilek@gmail.com>
2022-08-10 02:05:34 -0500

    Frontend Issues

--- GeneSearch/SearchEngine/views.py
+++ GeneSearch/SearchEngine/views.py
@@ -1,17 +1,20 @@
+from re import T
 from django.http import HttpResponse
 from django.template import loader 
 from .models import *
 
 
+
 def index(request):
-    X=Genestudy.objects.filter(approvedsymbol='RGS9')
+    X=Genestudy.objects.filter(approvedsymbol__contains='RGS9')
     context= {
         'listData':[]
     }
     for res in X: 
         context['listData'].append({
-            'Name':res.approvedname,
+            'HGNCID':res.approvedname,
             'Syn':res.synonyms,
+            
         })
     template=loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
